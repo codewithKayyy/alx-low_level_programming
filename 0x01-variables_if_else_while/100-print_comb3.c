70e6f8c5bf4a8202c7200b2d725127e2f210b0b7
codewithKayyy <mr.kayy2003@gmail.com>
2023-03-23 09:03:25 -0700

    initial commit

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+/**
+ * main - this is the main function
+ *
+ * Return: always 0
+ */
+
+int main(void)
+{
+	int i;
+	int x;
+
+	for (i = 0 ; i < 10 ; i++)
+	{
+		for (x = 1 ; x < 10 ; x++)
+		{
+			if (i < x && i != x)
+			{
+				putchar(i + '0');
+				putchar(x + '0');
+				if (i + x != 17)
+				{
+					putchar(',');
+					putchar(' ');
+				}
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
